7a6c016351217e8acd746620d28569230e4ba3e0
oluwadaprof <adeekotobiloba8@gmail.com>
2022-07-10 22:46:04 +0100

    updated data

--- src/components/Usercard/Usercard.jsx
+++ src/components/Usercard/Usercard.jsx
@@ -1,19 +1,32 @@
 import { Avatar } from '@material-ui/core'
 import React from 'react'
 import './usercard.scss'
+import data from '../../data/data'
 
 const Usercard = () => {
+ 
+
   return (
-    <div className='user' >
+     <div className='user' >
       <h1>Date</h1>
       <div className="user__card">
         <div className="avatar"><Avatar/> </div>
         <div className="content">
+          <div className="username">Adeeko Tobiloba</div>
+          <div className="status">Online</div>
+          <div className="telephone">+234-8126-01-9796</div>
+        </div>
+      </div>
+
+      {/* <div className="user__card">
+        <div className="avatar"><Avatar /> </div>
+        <div className="content">
           <div className="username">Adeeko Tobi</div>
           <div className="status">Online</div>
           <div className="telephone">+234-8126-01-9796</div>
         </div>
       </div>
+
 
       <div className="user__card">
         <div className="avatar"><Avatar /> </div>
@@ -31,7 +44,7 @@ const Usercard = () => {
           <div className="status">Online</div>
           <div className="telephone">+234-8126-01-9796</div>
         </div>
-      </div>
+      </div> */}
 
     </div>
   )
